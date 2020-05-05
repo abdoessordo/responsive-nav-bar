95bab85a894c097d3741b1bf50f26fc03586a61c
abdoessordo <62575309+abdoessordo@users.noreply.github.com>
2020-05-05 20:23:22 +0000

    Add files via upload

---
+++ codewars.js
@@ -0,0 +1,19 @@
+
+
+function sumDigPow(a, b) {
+    let array = []
+
+
+    for(var i=a ;i<=b; i++){
+        array.push(i)
+    }
+
+    for(var i=1 ;i<array.length; i++){
+
+    }  
+}
+
+sumDigPow(9,25)
+
+
+
